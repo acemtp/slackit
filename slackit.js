6f0a891c9fe5c597145f168a1ffbfec7988eeb98
Vianney Lecroart <acemtp@gmail.com>
2015-10-11 14:36:55 +0200

    packed

--- slackit.js
+++ slackit.js
@@ -1,93 +1,15 @@
-Infos = new Mongo.Collection('infos');
-if (Meteor.isClient) {
-  Template.registerHelper('info', () => Infos.findOne() );
-
-  Template.setup.events({
-    'submit form': function (e) {
-      const info = Infos.findOne();
-      Infos.upsert(info && info._id, { $set: { org: $('#org').val(), token: $('#token').val(), sentence: $('#sentence').val(), userCount: 'N/A', userOnlineCount: 'N/A' } });
-      return false;
-    },
-  });
-
-  Template.login.events({
-    'submit form': function (e) {
-      console.log('sub', $('#email').val());
-      $('#button').prop('disabled', true).removeClass('error').text('PLEASE WAIT!');
-      Meteor.call('j', $('#email').val(), err => {
-        if(err) {
-          console.log('err', err);
-          $('#button').prop('disabled', false).addClass('error').text(err.error);
-        } else {
-          $('#button').addClass('success').text('WOOT. CHECK YOUR EMAIL!');
-        }
-      });
-      return false;
-    },
-  });
-}
-
-if (Meteor.isServer) {
-
-  Infos.allow({
-    insert() { return Infos.find().count() === 0; }
-  });
-
-  Meteor.publish(null, () => {
-    return Infos.find({}, { fields: { token: 0 } });
-  });
-
-  Meteor.methods({
-    j(email) {
-      const info = Infos.findOne();
-      let res;
-      try {
-        res = HTTP.post(`https://${info.org}.slack.com/api/users.admin.invite`, {
-          params: {
-             email,
-             token: info.token,
-          }
-        });
-      } catch(e) {
-        console.log('eee', e);
-        throw new Meteor.Error('yolo2', 'boom2');
-      }
-      if(res && res.data && res.data.ok) {
-        console.log('aa', res);
-      } else {
-        console.error('boom', res);
-        throw new Meteor.Error(res.data.error);
-      }
-    },
-  });
-
-  const update = () => {
-    const info = Infos.findOne();
-    if(info) {
-      try {
-        const res = HTTP.post(`https://${info.org}.slack.com/api/users.list`, {
-          params: {
-             token: info.token,
-             presence: 1,
-          }
-        });
-        if(res && res.data && res.data.ok) {
-          //console.log('aa', res);
-
-          Infos.update(info._id, { $set: {
-            userCount: res.data.members.length,
-            userOnlineCount: _.where(res.data.members, { presence: 'active' }).length,
-          } });
-
-        } else {
-          console.error('boom', res);
-        }
-      } catch(e) {
-        console.log('eee', e);
-      }
-    }
-    Meteor.setTimeout(update, 1000);
-  };
-
-  update();
-}
+a='addClass';z=()=>y.findOne();y=new Mongo.Collection('i');if(Meteor.isClient){
+Template.registerHelper('i',()=>{return z()});Template.s.events({'submit #f'(){
+Meteor.call('s',{o:$('#o').val(),t:$('#t').val(),s:$('#s').val()});return false
+}});Template.l.events({'submit #f'(){$('#b').prop('disabled',true).removeClass(
+'erro').text('PLEASE WAIT');Meteor.call('j',$('#e').val(),e=>{if(e)$('#b').prop
+('disabled',false)[a]('erro').text(e.error);else $('#b')[a]('succes').text(`CH\
+ECK YOUR EMAIL`)});return false}})}if(Meteor.isServer){Meteor.publish(null,()=>
+{return y.find({},{fields:{t:0}})});Meteor.methods({s($set){i=z();y.upsert(i&&i
+._id,{$set})},j(email){let i=z(),r;try{r=HTTP.post(`https://${i.o}.slack.com/a\
+pi/users.admin.invite`,{params:{email,token:i.t}})}catch(e){}if(!r||!r.data||!r
+.data.ok)throw new Meteor.Error(r.data.error)}});let u=()=>{let i=z();if(i){try
+{let r=HTTP.post(`https://${i.o}.slack.com/api/users.list`, {params:{token:i.t,
+presence:1}});if(r.data.ok) {y.update(i._id,{$set:{u:r.data.members.length,v:_.
+where(r.data.members,{presence:'active'}).length}})}}catch(e){}}Meteor[`setTim\
+eout`](u, 1000)};u()}//goodlucktounderstandanddebugthatlittlehacker.gometeorgo.
